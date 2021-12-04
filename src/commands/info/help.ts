2b84fa7f2d572791c4d1909ce77fb06d6fb6b8f7
Ali EL AMRI <shoodey@gmail.com>
2021-12-04 18:39:42 +0200

    feat(help command): added help command

--- src/commands/info/help.ts
+++ src/commands/info/help.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, EmbedFieldData, MessageEmbed } from "discord.js";
 import Client from "../../entities/client";
 import {
   Command,
@@ -9,7 +9,82 @@ import {
 const execute: ExecuteFunction = async (
   client: Client,
   interaction: CommandInteraction
-) => {};
+) => {
+  const option = interaction.options.get("command")?.value as string;
+  const commandToHelpWith = client.commands
+    .filter((command: Command) => command.category !== CommandCategory.admin)
+    .get(option?.toLowerCase());
+
+  let messageEmbed: MessageEmbed;
+  if (!commandToHelpWith) {
+    const fields: Array<EmbedFieldData> = [...client.categories]
+      .filter((category: string) => category !== CommandCategory.admin)
+      .map((category: string) => {
+        const commandCount = client.commands.filter(
+          (command: Command) => command.category === category
+        ).size;
+
+        return {
+          name: `${category} [${commandCount}]`,
+          value: client.commands
+            .filter((command: Command) => command.category === category)
+            .map((command: Command) => `\`/${command.name}\``)
+            .join(", "),
+        };
+      });
+
+    messageEmbed = client.embed(
+      {
+        title: `**${client.user?.username}** offers **${
+          client.commands.filter(
+            (value: Command) => value.category !== CommandCategory.admin
+          ).size
+        }** commands!`,
+        url: "https://github.com/Shoodey/chill-pill",
+        fields,
+        description:
+          "Feel like coming up additional ones? Feel free to contibute to the [GitHub repo](https://github.com/Shoodey/chill-pill) and star it!",
+      },
+      interaction
+    );
+
+    return interaction.reply({
+      embeds: [messageEmbed],
+      ephemeral: command.ephemeral,
+    });
+  }
+
+  messageEmbed = client.embed(
+    {
+      title: `Help on \`/${commandToHelpWith.name}\``,
+      fields: [
+        {
+          name: "Please note...",
+          value: "Options: ?:optional !:required <...>:type",
+          inline: true,
+        },
+      ],
+      description: `
+           **Name:** \`${commandToHelpWith.name}\`
+           **Aliases:** \`${
+             commandToHelpWith.aliases && commandToHelpWith.aliases.length
+               ? commandToHelpWith.aliases.join(",")
+               : `none`
+           }\`
+           **Category:** \`${commandToHelpWith.category}\`
+           **Description:** \`${commandToHelpWith.description}\`
+           **Usage:** \`${commandToHelpWith.usage}\`
+           **Cooldown:** \`${commandToHelpWith.cooldown}s\`
+        `,
+    },
+    interaction
+  );
+
+  return interaction.reply({
+    embeds: [messageEmbed],
+    ephemeral: command.ephemeral,
+  });
+};
 
 const command: Command = {
   name: "help",
@@ -22,7 +97,7 @@ const command: Command = {
     },
   ],
   ephemeral: true,
-  cooldown: 0,
+  cooldown: 3,
   category: CommandCategory.info,
   description: "Lists available useful (or not) commands.",
   usage: "/help ?command",
