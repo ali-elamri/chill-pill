67e88420b89579acc45c3b1f72e26202cfaa9e9b
Ali EL AMRI <shoodey@gmail.com>
2021-12-04 14:54:47 +0200

    feat(commands): added a ping command
    can either be used as ephemeral or not to add reactions

--- src/commands/info/ping.ts
+++ src/commands/info/ping.ts
@@ -1,4 +1,5 @@
-import { CommandInteraction } from "discord.js";
+import { CommandInteraction, Message } from "discord.js";
+import { promisify } from "util";
 import Client from "../../entities/client";
 import {
   Command,
@@ -6,10 +7,48 @@ import {
   ExecuteFunction,
 } from "../../interfaces/command";
 
+const wait = promisify(setTimeout);
+
 const execute: ExecuteFunction = async (
   client: Client,
   interaction: CommandInteraction
-) => {};
+) => {
+  const message = await interaction.reply({
+    embeds: [
+      client.embed(
+        {
+          title: `⏳ Pinging...`,
+          description: `We will get you a response shortly...`,
+        },
+        interaction
+      ),
+    ],
+    ephemeral: command.ephemeral,
+    fetchReply: true,
+  });
+
+  if (!command.ephemeral && message instanceof Message) {
+    await message.react("🇵");
+    await message.react("🇮");
+    await message.react("🇳");
+    await message.react("🇬");
+    await message.react("💊");
+  } else {
+    await wait(1000);
+  }
+
+  await interaction.editReply({
+    embeds: [
+      client.embed(
+        {
+          title: `:white_check_mark: It's alive!`,
+          description: `There you go!`,
+        },
+        interaction
+      ),
+    ],
+  });
+};
 
 const command: Command = {
   name: "ping",
