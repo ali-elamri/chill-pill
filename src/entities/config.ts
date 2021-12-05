ec5e6c79a21a3f5636e3ec7b614379375406729e
Ali EL AMRI <shoodey@gmail.com>
2021-12-05 03:05:48 +0200

    feat(firebase): added firebase to use firestore as a real-time storage

--- src/entities/config.ts
+++ src/entities/config.ts
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { env } from "process";
 
 dotenv.config();
 
@@ -12,12 +13,32 @@ const parseServers = (server_ids: string): string[] => {
   return servers;
 };
 
+export interface FirebaseConfig {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+  measurementId: string;
+}
+
 export interface Config {
   client_token: string;
   guilds: string[];
+  firebase: FirebaseConfig;
 }
 
 export const config = {
   client_token: process.env.CLIENT_TOKEN!,
   guilds: parseServers(process.env.SERVER_IDS!),
+  firebase: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  },
 } as Config;
---
+++ src/entities/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  DocumentData,
+  CollectionReference,
+} from "firebase/firestore";
+import { config } from "./config";
+
+export const firebaseApp = initializeApp(config.firebase);
+
+export const firestore = getFirestore();
+
+export const createCollection = <T = DocumentData>(collectionName: string) => {
+  return collection(firestore, collectionName) as CollectionReference<T>;
+};
